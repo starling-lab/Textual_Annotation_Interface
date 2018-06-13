abfcd99c112f1a6f8c4b4331e37213bc6cb626d5
Abhiramon <Abhiramon.R@iiitb.org>
2018-06-13 17:33:08 -0500

    cleanup updated

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 bash ./test_setup.sh
 
-java -jar v1-0.jar -i -model train/models/ -test test/ -target sentenceContainsTarget -trees 25 -aucJarPath . > test.log 2> test_error.log
+java -jar v1-0.jar -i -model train/models/ -test test/ -target sentenceContainsTarget -trees 25 -aucJarPath . > test.log 2> test-error.log
