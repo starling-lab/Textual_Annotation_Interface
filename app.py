f85f1285b794ee08fe1f5f55db15450642f7fd4d
batflyer <alexander@batflyer.net>
2018-06-19 19:04:52 -0500

    Python 2/3 compatability for app.py

--- app.py
+++ app.py
@@ -1,103 +1,114 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from flask import Flask, render_template
 from nltk import sent_tokenize
-import rnlp
+
+import argparse
 import os
+import rnlp
 import sys
 
 train_pos_src = "files/pos_train_examples.txt"
 test_pos_src = "files/pos_test_examples.txt"
 
-if len(sys.argv) > 1:
-	train_document_src = sys.argv[1]
-	test_document_src = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('-train', type=str,
+                    required=True, help='Path to training document')
+parser.add_argument('-test', type=str,
+                    required=True, help='Path to testing document')
+args = parser.parse_args()
 
-	print "Path of train document: "+sys.argv[1]
-	print "Path of test document: "+sys.argv[2]
-else:
-	print "Please enter path of train document: "
-	train_document_src = raw_input()
-	print "Please enter path of test document: "
-	test_document_src = raw_input()
-
+train_document_src = args.train
+test_document_src = args.test
 
 # train_document_src = "files/document.txt"
 # test_document_src = "files/document5.txt"
 
-train_pos_file = open(train_pos_src,'a')
+train_pos_file = open(train_pos_src, 'a')
 
 test_pos_file = open(test_pos_src, 'a')
 
 app = Flask(__name__)
 
+
 @app.route("/addTextTrain/<jsdata>")
-#Change this to post to improve security
+# Change this to post to improve security
 def add_positive_example(jsdata):
-	global train_pos_file
-	train_pos_file.write(jsdata+"\n")
-	return jsdata
+    global train_pos_file
+    train_pos_file.write(jsdata+"\n")
+    return jsdata
+
 
 @app.route("/addTextTest/<jsdata>")
-#Change this to post to improve security
+# Change this to post to improve security
 def add_positive_example_test(jsdata):
-	global test_pos_file
-	test_pos_file.write(jsdata+"\n")
-	return jsdata
+    global test_pos_file
+    test_pos_file.write(jsdata+"\n")
+    return jsdata
 
 
 def get_pos_examples(pos_examples_file, pos_examples_src):
-	pos_examples_file.close()
-	pos_examples_file = open(pos_examples_src, "r")
-	pos_examples = pos_examples_file.read().split('\n')
-	pos_examples = [e for e in pos_examples if e != '']
-	return pos_examples
+    pos_examples_file.close()
+    pos_examples_file = open(pos_examples_src, "r")
+    pos_examples = pos_examples_file.read().split('\n')
+    pos_examples = [e for e in pos_examples if e != '']
+    return pos_examples
+
 
 def document_to_lines(document):
-	lines = []
-	paras = document.split('\n')
-	for para in paras:
-		# lines.extend(para.split('.'))
-		para_lines = para.split('.')
-		for para_line in para_lines:
-			para_line+="."
-			lines.append(para_line)
-	return lines
-
+    lines = []
+    paras = document.split('\n')
+    for para in paras:
+        # lines.extend(para.split('.'))
+        para_lines = para.split('.')
+        for para_line in para_lines:
+            para_line += "."
+            lines.append(para_line)
+    return lines
 
 
 def get_pos_lines(document, examples):
+	"""
+    Assumptions:
 
-	#Returns the lines in document containing the example phrases
-	'''Assumptions: 1. Examples are given in top-down order in the document.
-		2. Multiple examples can be given for a single line in document (The line is added only once)
-		3. If the same example is encountered again, it is marked again. 
-		4. If the same line appears in the document containing example/s, it will be added again. 
-	'''
+    1. Examples are given in top-down order in the document.
+    2. Multiple examples can be given for a single line in the document
+       (The line is added only once).
+    3. If the same example is encountered again, it is marked again.
+    4. If the same line appears in the document containing examples, it will
+       be added again.
+    """
+
 	pos_lines = []
 	temp = []
 	line_index = 0
 	example_index = 0
 
+	import pdb; pdb.set_trace()
+
 	while(example_index < len(examples)):
-		while(line_index< len(document)):
-			if (example_index >=len(examples)):
+		while(line_index < len(document)):
+			if (example_index >= len(examples)):
 				break
 			example = examples[example_index]
 			line = document[line_index]
 
 			if example in line:
-				if len(pos_lines)>0 and pos_lines[-1] == line:
-					#The next example is also present in previous line or same line repeats
-					example_index+=1
+				if len(pos_lines) > 0 and pos_lines[-1] == line:
+					# The next example is also present in the previous line.
+					# or the same line repeats.
+					example_index += 1
 				else:
 					temp.append(line)
-					example_index+=1
+					example_index += 1
 			else:
-				line_index+=1
+				line_index += 1
+
 	for t in temp:
 		pos_line = t.strip()
 		pos_lines.append(pos_line)
 	return pos_lines
-
 
 
 def create_files(corpus, labeled_positive):
@@ -145,7 +156,6 @@ def create_files(corpus, labeled_positive):
 	        f.write(n + '\n')
 
 
-
 @app.route("/learn/")
 def learn():
 	global train_document_src
@@ -157,7 +167,7 @@ def learn():
 	pos_lines = get_pos_lines(line_list,examples)
 	create_files(document,pos_lines)
 	os.system("bash train.sh")
-	print "Training complete!!\n"
+	print("Training complete!!\n")
 	return ""
 
 
@@ -172,7 +182,7 @@ def test():
 	pos_lines = get_pos_lines(line_list,examples)
 	create_files(document,pos_lines)
 	os.system("bash test.sh")
-	print "Testing done"
+	print("Testing done")
 	return ""
 
 
