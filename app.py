8377279a8b956a7a40b815be1352af2ad319ddcb
Abhiramon <Abhiramon.R@iiitb.org>
2018-06-21 15:05:21 -0500

    Comments added

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import rnlp
 import os
 import sys
 
+#The code uses GET methods to communicate from JS to app.py. This may be misused. Alternative method of communication can be used to improve security
 
 train_pos_src = "files/pos_train_examples.txt"
 test_pos_src = "files/pos_test_examples.txt"
 
 block_size = 2
 
+#Taking train and test documents as commandline or terminal inputs
 if len(sys.argv) > 1:
 	train_document_src = sys.argv[1]
 	test_document_src = sys.argv[2]
@@ -22,31 +24,16 @@ else:
 	print "Please enter path of test document: "
 	test_document_src = raw_input()
 
-
-# train_document_src = "files/document.txt"
-# test_document_src = "files/document5.txt"
-
+#Preparing filess
 train_pos_file = open(train_pos_src,'a')
 
 test_pos_file = open(test_pos_src, 'a')
 
-app = Flask(__name__)
-
-@app.route("/addTextTrain/<jsdata>")
-#Change this to post to improve security
-def add_positive_example(jsdata):
-	global train_pos_file
-	train_pos_file.write(jsdata+"\n")
-	return jsdata
-
-@app.route("/addTextTest/<jsdata>")
-#Change this to post to improve security
-def add_positive_example_test(jsdata):
-	global test_pos_file
-	test_pos_file.write(jsdata+"\n")
-	return jsdata
+# Helper functions -------------------------
 
 def get_scores(file_src):
+	# Output: Returns the list of dictionaries. Each dictionary corresponds to a sentence in the test doc. Each dict has fields, neg (boolean: is example negative?), score (regression score for match), block_id, sentence_id
+	# Input: Path of the file containing the results (results_sentenceContainsTarget.db)
 	global block_size
 
 	file = open(file_src, "r")
@@ -66,7 +53,7 @@ def get_scores(file_src):
 			dict["block_id"] = int(terms[0][i1:i2].split('_')[0])
 			dict["sentence_id"] = int(terms[0][i1:i2].split('_')[1])
 			dict_list.append(dict)
-	#Re-arranging dict list
+	#Re-arranging dict list in order of sentences in doc
 	lst = []
 	block_id = 1
 	sentence_id = 1
@@ -83,6 +70,7 @@ def get_scores(file_src):
 
 
 def get_pos_examples(pos_examples_file, pos_examples_src):
+	#Returns a list of positive examples read from file
 	pos_examples_file.close()
 	pos_examples_file = open(pos_examples_src, "r")
 	pos_examples = pos_examples_file.read().split('\n')
@@ -90,6 +78,7 @@ def get_pos_examples(pos_examples_file, pos_examples_src):
 	return pos_examples
 
 def document_to_lines(document):
+	# Returns a list of sentences in a document string (input)
 	lines = []
 	paras = document.split('\n')
 	for para in paras:
@@ -139,6 +128,7 @@ def get_pos_lines(document, examples):
 
 
 def create_files(corpus, labeled_positive):
+
 	example_corpus = corpus.split('\n')
 	corpus_string = ""
 	for sentence in example_corpus:
@@ -182,8 +172,35 @@ def create_files(corpus, labeled_positive):
 	    for n in negative:
 	        f.write(n + '\n')
 
+# End of helper functions -------------------------------------
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	#Create and render index page
+	global train_document_src
+	global test_document_src
+	return render_template("index.html").format(open(train_document_src,"r").read(),open(test_document_src,"r").read())
+
+@app.route("/addTextTrain/<jsdata>")
+def add_positive_example(jsdata):
+	#Adds phrase as positive example to file (train)
+	global train_pos_file
+	train_pos_file.write(jsdata+"\n")
+	return jsdata
+
+@app.route("/addTextTest/<jsdata>")
+def add_positive_example_test(jsdata):
+	#Adds phrase as positive example to file (test)
+	global test_pos_file
+	test_pos_file.write(jsdata+"\n")
+	return jsdata
+
+
 @app.route("/result", methods = ["POST","GET"])
 def result():
+	#Render results page
 	global test_document_src
 	text = ""
 	scores = get_scores("test/results_sentenceContainsTarget.db")
@@ -199,6 +216,7 @@ def result():
 
 @app.route("/learn/")
 def learn():
+	#Train model using files created
 	global train_document_src
 	global train_pos_file
 	global train_pos_src
@@ -214,6 +232,7 @@ def learn():
 
 @app.route("/test/")
 def test():
+	#Test model
 	global test_document_src
 	global test_pos_file
 	global test_pos_src
@@ -226,13 +245,6 @@ def test():
 	print "Testing done!!"
 	return ""
 	
-
-
-@app.route("/")
-def index():
-	global train_document_src
-	global test_document_src
-	return render_template("index.html").format(open(train_document_src,"r").read(),open(test_document_src,"r").read())
 
 if __name__ == '__main__':
 	app.run(debug =True)
