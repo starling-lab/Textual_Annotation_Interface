6406533cbcecccdb541168b11b01d7bdaf06687f
Abhiramon <Abhiramon.R@iiitb.org>
2018-06-24 22:26:57 -0500

    Automate plotting first two models

---
+++ plot_models.sh
@@ -0,0 +1,4 @@
+mkdir images
+cd train/models/bRDNs/dotFiles
+dot -Tpng WILLTreeFor_sentenceContainsTarget0.dot -o ../../../../images/0.png
+dot -Tpng WILLTreeFor_sentenceContainsTarget1.dot -o ../../../../images/1.png
